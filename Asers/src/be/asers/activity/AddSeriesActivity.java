e83e3011c806e3c74d535e003ddd79ad0c23b04e
chesteric31 <chesteric31@gmail.com>
2013-10-24 15:49:54 +0200

    Restructure activities forms.

--- Asers/src/be/asers/activity/AddSeriesActivity.java
+++ Asers/src/be/asers/activity/AddSeriesActivity.java
@@ -10,7 +10,6 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
